70a9a8a317398274c07ceb1ebd2a0e1613f19232
patrik.akselsson <patrik.akselsson@valtech.se>
2011-02-27 00:41:42 +0100

    Added support for command line arguments to cucumber

--- parallel_specs.rb
+++ parallel_specs.rb
@@ -1,6 +1,8 @@
 require 'optparse'
 
-options = {}
+options = {
+    :extra_arguments => []
+}
 OptionParser.new do |opts|
   opts.banner = "Usage: #{__FILE__} [options]"
 
@@ -12,6 +14,10 @@ OptionParser.new do |opts|
     options[:features] = f
   end
 
+  opts.on("--args x,y,z",Array,"Arguments passed to cucumber") do |args|
+    options[:extra_arguments] << args
+  end
+
   opts.on("-h", "--help", "Show this message") do
     puts opts
     exit
@@ -20,15 +26,16 @@ end.parse!
 
 Dir.chdir options[:path] if options.has_key? :path
 features = options[:features] || Dir.glob("**/*.feature")
+extra_arguments = options[:extra_arguments].join(" ")
 
 processes = features.map do |f|
-  puts "Starting #{f}"
-  IO.popen("cucumber " + f)
+  puts "Running #{f}"
+  IO.popen("cucumber #{f} #{extra_arguments}")
 end
 
 begin
   results = Array.new(processes.length)
-  collectors = processes.map {|r| Thread.start {results[processes.index(r)] = r.readlines}}
+  collectors = processes.map {|p| Thread.start {results[processes.index(p)] = p.readlines}}
   collectors.each {|c| c.join}
   results.each { |r| puts "", r }
 rescue SystemExit, Interrupt
